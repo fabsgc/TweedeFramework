6a7a2fdc6673d3b868c465eefe21d64c0853a88d
fabsgc <fabienbeaudimi@hotmail.fr>
2018-11-13 00:14:35 +0100

    Update cmake config + working on utilities

---
+++ Source/Framework/Utility/Error/TeConsole.cpp
@@ -0,0 +1,34 @@
+#include "Error/TeConsole.h"
+
+namespace te
+{
+    void Console::OnStartUp()
+    {
+#if TE_DEBUG_MODE == 1 && TE_PLATFORM == TE_PLATFORM_WIN32
+        AllocConsole();
+        AttachConsole(GetCurrentProcessId());
+
+        HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
+        _hCrtOut = _open_osfhandle((intptr_t)handle_out, _O_TEXT);
+        FILE* hf_out = _fdopen(_hCrtOut, "w");
+        setvbuf(hf_out, NULL, _IONBF, 1);
+        *stdout = *hf_out;
+
+        FILE *fpstdout = stdout, *fpstderr = stderr;
+
+        freopen_s(&fpstdout, "CONOUT$", "w", stdout);
+        freopen_s(&fpstderr, "CONOUT$", "w", stderr);
+#endif
+    }
+
+    void Console::OnShutDown()
+    {
+#if TE_DEBUG_MODE == 1 && TE_PLATFORM == TE_PLATFORM_WIN32
+#endif
+    }
+
+    Console& gConsole()
+    {
+        return static_cast<Console&>(Console::Instance());
+    }
+}
