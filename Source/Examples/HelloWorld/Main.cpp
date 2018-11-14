c876180786cad15ec74491f9531b2fe0fd121b2e
fabsgc <fabienbeaudimi@hotmail.fr>
2018-11-14 23:42:15 +0100

    Working on base code for Application and Window (will work on WIN32 window)

--- Source/Examples/HelloWorld/Main.cpp
+++ Source/Examples/HelloWorld/Main.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include "Prerequisites/TePrerequisitesUtility.h"
-#include "String/TeString.h"
-
 #include "TeCoreApplication.h"
 
 #if TE_PLATFORM == TE_PLATFORM_WIN32
@@ -17,11 +13,6 @@ int CALLBACK WinMain(
 int main()
 #endif
 {
-    te::String hello = "Hello World ";
-    hello.append("my friend !");
-
-    std::cout << hello << std::endl;
-
     te::START_UP_DESC desc;
 
     te::CoreApplication::StartUp(desc);
